d18d09a42a82bb65ad9a7ec3f2d47c6a5fb2d94c
Jack Clarke <j.clarke@student.unsw.edu.au>
2020-03-31 16:23:42 +1100

    first test of Loop

---
+++ LoopGffRemove.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+folders=(2020-03-27.ebs10xv2.ncbi.Gnomon.gff3_Extracts/             2020-03-27.ts10xv2.ncbi.Gnomon.gff3_Extracts/                                 2020-03-31.GCF_001077635.1_Thamnophis_sirtalis-6.0_genomic_Extracts/
+2020-03-27.GCA_000516915.1_OphHan1.0_genomic_Extracts/     2020-03-30.GCF_900067755.1_pvi1.1_genomic_Extracts/                           2020-03-31.GCF_001527695.2_P.Mucros_1.0_genomic_Extracts/
+2020-03-27.GCF_009769535.1_rThaEle1.pri_genomic_Extracts/  2020-03-31.GCF_000186305.1_Python_molurus_bivittatus-5.0.2_genomic_Extracts/)
+spec=( PSETE OPHHA THAEL NOTSC POVIL PYTBI THASI PROMU)
+echo ${spec[0]}
+i=0
+input=/scratch/venom/SnakeVenom-Feb20/Data/RepltileDBFilter/ReptileGenomes.prot.fa
+for fold in ${folders[@]}
+do
+    echo $fold 
+    echo ${spec[i]}
+    echo ${input}
+    python ~/progs/SnakeGit/GffDuplicateSearch.py \
+        ${fold}*.gene.txt  \
+        ${fold}*.RNA.txt \
+        ${fold}*.CDS.txt \
+        ${input} \
+        /scratch/venom/SnakeVenom-Feb20/Data/RepltileDBFilter/ReptileGenomes.prot.${spec[i]}.fa  > ${spec[i]}.out
+    i=$((i +1 ))
+    input=/scratch/venom/SnakeVenom-Feb20/Data/RepltileDBFilter/ReptileGenomes.prot.${spec[i]}.fa
+done
+

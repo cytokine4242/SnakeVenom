c5a73ca28b1db01f272ca60974f6c07eec65d7a1
Jack Clarke <z5162865@unsw.edu.au>
2020-06-03 12:28:38 +1000

    added renamescaffsolds that workds but is very slow

---
+++ renammeScaffs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+mapping=$(cat $1)
+gff=$2
+
+out=$3
+cp $2 $3
+#echo "cat out"
+#cat $out
+for line in $mapping
+do
+        #cat  $out
+	#echo $line
+	NCBI=$(echo $line | cut -f1 -d',')
+	BABS=$(echo $line | cut -f2 -d',')
+	echo $BABS
+	echo $NCBI
+	#cat $tmp
+	sed -i "s/$BABS/$NCBI/g" $out 
+
+
+
+done 

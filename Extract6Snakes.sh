d01d5b515af3261d254bc0d2e7d3cd9c3dec22bf
Jack Clarke <j.clarke@student.unsw.edu.au>
2020-07-02 14:21:37 +1000

    added extract 6 snakes code

--- Extract6Snakes.sh
+++ Extract6Snakes.sh
@@ -3,7 +3,9 @@ fasta=$(ls *.fas)
 zero=0
 for i in $fasta
 do
-    name=$(basename $i .fas)
+
+    date=$(basename $i .fas)
+    name=${date:11}"
 	egrep -A1 "CROVV|HYDCUR|NAJNA|BOACO|PSETE|NOTSC" $i > ${out}/${name}.6Snake.fas
 
 done 
